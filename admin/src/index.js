e94303d6bb49800b0558ef4ac964341a99916d53
constSnackOverflow <inshirah.akhan@gmail.com>
2023-05-31 13:08:25 +0100

    Feat: Add test endpoint to retrieve all financial companies

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,17 +1,29 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const {investmentsServiceUrl, port} = require("config")
+const {financialsServiceUrl, investmentsServiceUrl, port} = require("config")
 const {get: fetch} = require("axios")
 const {get, listen, use} = express()
 
 use(bodyParser.json({limit: "10mb"}))
 
+// Endpoint to fetch all investments for a given user
 get("/investments/:id", async ({params: {id}}, res) => {
   try {
     const {data: investmentsById} = await fetch(
       `${investmentsServiceUrl}/investments/${id}`,
     )
     res.status(200).send(investmentsById)
+  } catch (error) {
+    console.error(error)
+    res.send(500)
+  }
+})
+
+// Endpoint to fetch all financial companies
+get("/financial-companies", async (req, res) => {
+  try {
+    const {data: financialCompanies} = await fetch(`${financialsServiceUrl}/companies`)
+    res.status(200).send(financialCompanies)
   } catch (error) {
     console.error(error)
     res.send(500)
